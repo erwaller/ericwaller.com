a82382d7fbcef9504a98cee8ad761fee8a0f541e
Eric Waller <erwaller@gmail.com>
2012-08-01 00:48:45 -0400

    quick homepage

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+app.debug = os.environ.get("APP_ENV") == "dev"
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('APP_ENV') == 'dev'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
