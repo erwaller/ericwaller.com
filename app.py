78511616bfca709b387cf7b14d571ea9e260aaac
Eric Waller <erwaller@gmail.com>
2012-07-31 23:15:17 -0400

    setup dev/prod

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    print port
-    app.run(host='0.0.0.0', port=port)
+    debug = os.environ.get('APP_ENV') == 'dev'
+    app.run(host='0.0.0.0', port=port, debug=debug)
