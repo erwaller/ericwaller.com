162cd22cd4ec53e0c3c773d2698b1c52aa2511ce
Eric Waller <erwaller@gmail.com>
2017-09-18 15:35:50 -0400

    Add keybase verification

--- app.py
+++ app.py
@@ -35,6 +35,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/keybase.txt")
+def keybase():
+    return render_template("keybase.txt")
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
